348a59793588701f3e805794943acbd13aaec0a0
cillogee <cilliangrady@gmail.com>
2026-03-04 10:44:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById('button');
 
-document.getElementById('form')
+document.getElementById('orderform')
  .addEventListener('submit', function(event) {
    event.preventDefault();
 
